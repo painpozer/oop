08e5f66c64efea974d88be1fb0168da9baa0c3b6
Андрей Половинкин <polovinkin002@outlook.com>
2026-09-23 23:22:50 +0500

    задокументировал код

--- lab1/lab11.cpp
+++ lab1/lab11.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * @brief функция заполняет массив случайными числами
+ * @param arr массив из 10 чисел
+ */
+
 void fillArray(int (&arr)[10]) {
     for (int i = 0; i < 10; i++) {
         arr[i] = rand() % 100;
     }
 }
+
 
 int main()
 {
