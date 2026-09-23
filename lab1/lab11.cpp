1c3216de8726b0e1e342a9442974789eb1ac7b88
Андрей Половинкин <polovinkin002@outlook.com>
2026-09-24 01:00:16 +0500

    добавлена функция printArray которая выводит элеманты массива

--- lab1/lab11.cpp
+++ lab1/lab11.cpp
@@ -11,15 +11,20 @@ void fillArray(int (&arr)[10]) {
         arr[i] = rand() % 100;
     }
 }
-
+/**
+ * @brief функция выводит массив на экран
+ * @param arr массив из 10 чисел
+ */
+void printArray(int (&arr)[10]) {
+    for (auto i : arr) {
+        cout << i << " ";
+    }
+}
 
 int main()
 {
     int arr[10];
     fillArray(arr);
-    for (int i = 0; i < 10; i++) {
-        cout << arr[i] << " ";
-
-    }
+    printArray(arr);
     return 0;
 }
