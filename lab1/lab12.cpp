101983f8ed7f1336bac49aae82235e7e5dbc4578
Андрей Половинкин <polovinkin002@outlook.com>
2026-09-24 09:13:14 +0500

    добавлена функция process которая находит первый отрицательный элемент массивва

--- lab1/lab12.cpp
+++ lab1/lab12.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
 using namespace std;
+
+
+/**
+ * @brief функция заполняет массив случайными значениями от -50 до 49
+ * @param size размер массива
+ * @param arr указатель массива из n чисел
+ *
+ */
+void fillArray(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100 - 50;
+    }
+}
+
+/**
+ * @brief ищет первый отриц элемент массива
+ * @param size размер массива
+ * @param arr указатель массива из n чисел
+ */
+void process(int*& arr, int size) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            cout << "первый отрицательный элемент: " << arr[i] << endl;
+            break;
+        }
+    }
+}
+
+/**
+ * @brief функция выводит массив на экран
+ * @param size размер массива
+ * @param arr указатель массива из n чисел
+ */
+void printArray(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
 /**
  * @brief функция получает размер массива и выделяет под него память
- * @param n размер массива
- * @param arr динамический массив из n чисел
  *
  */
 int main ()
@@ -12,6 +49,13 @@ int main ()
     cout << "введите размер массива  ";
     cin >> n;
     cout << n;
+    cout << endl;
     int* arr = new int[n];
+    fillArray(arr, n);
+    printArray(arr, n);
+    cout << endl;
+    process(arr, n);
+
+
 
 }
