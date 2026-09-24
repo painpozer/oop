4577aaecb14015bf5a437518928a38abd28bd585
Андрей Половинкин <polovinkin002@outlook.com>
2026-09-24 08:35:52 +0500

    добавили динамическое выделение памяти под массив

--- lab1/lab12.cpp
+++ lab1/lab12.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
-
+/**
+ * @brief функция получает размер массива и выделяет под него память
+ * @param n размер массива
+ * @param arr динамический массив из n чисел
+ *
+ */
 int main ()
 {
     int n;
     cout << "введите размер массива  ";
     cin >> n;
     cout << n;
+    int* arr = new int[n];
+
 }
