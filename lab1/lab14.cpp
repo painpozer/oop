4c97848bb7bd9b0d0eebc4787c6a8d22541faa77
Андрей Половинкин <polovinkin002@outlook.com>
2026-09-24 16:28:00 +0500

    написал функцию красивого вывода

--- lab1/lab14.cpp
+++ lab1/lab14.cpp
@@ -29,12 +29,53 @@ void fillMatrix(int** matrix, int rows, int cols) {
     }
 }
 
+/**
+ * @brief Красиво выводит матрицу на экран
+ * @param matrix Указатель на матрицу
+ * @param rows Количество строк
+ * @param cols Количество столбцов
+ * @param showBorders Рисовать ли рамку из '*' (по умолчанию true)
+ * @param title Заголовок над матрицей (по умолчанию "Matrix")
+ */
+void printMatrix(int** matrix, int rows, int cols, bool showBorders = true, string title = "Matrix") {
+    cout << "\n ||| " << title << " |||\n";
+
+    if (showBorders) {
+        cout << "*";
+        for (int j = 0; j < cols; ++j) {
+            cout << "******";
+        }
+        cout << "*\n";
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        if (showBorders) cout << "*";
+        for (int j = 0; j < cols; ++j) {
+            cout << " " << matrix[i][j];
+            int len = to_string(matrix[i][j]).length();
+            for (int k = len; k < 4; ++k) cout << " ";
+            if (showBorders) cout << "*";
+        }
+        cout << "\n";
+    }
+
+    if (showBorders) {
+        cout << "*";
+        for (int j = 0; j < cols; ++j) {
+            cout << "******";
+        }
+        cout << "*\n";
+    }
+    cout << endl;
+}
+
 int main() {
 
-    int rows = 2;
+    int rows = 7;
     int cols = 2;
     int** myMatrix = allocateMatrix(rows, cols);
     fillMatrix(myMatrix, rows, cols);
+    printMatrix(myMatrix, rows, cols, true);
 
     return 0;
 
