9e3c5624b3da82e55ee16fe54882b2b9fe487b7f
Андрей Половинкин <polovinkin002@outlook.com>
2026-09-24 16:18:16 +0500

    написал функцию заполнения void fillMatrix

--- lab1/lab14.cpp
+++ lab1/lab14.cpp
@@ -7,19 +7,34 @@ using namespace std;
 * @param cols Количество столбцов
 **/
 int** allocateMatrix(int rows, int cols) {
-    int** matrix = new int*[rows]{};
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = new int[cols]{};
+    int** matrix = new int*[rows];
+    for (int i = 0; i < rows; ++i) {
+        matrix[i] = new int[cols];
+    }
+    return matrix;
+}
+
+/**
+* @brief функция заполнения
+* @param matrix Наша матрица
+* @param rows Количество строк
+* @param cols Количество столбцов
+**/
+void fillMatrix(int** matrix, int rows, int cols) {
+    cout << "введи " << rows * cols << " элементов" << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cin >> matrix[i][j]; // Теперь здесь память существует, SIGBUS не будет!
+        }
     }
 }
 
-
-
 int main() {
 
-    int rows = 3;
-    int cols = 3;
+    int rows = 2;
+    int cols = 2;
     int** myMatrix = allocateMatrix(rows, cols);
+    fillMatrix(myMatrix, rows, cols);
 
     return 0;
 
