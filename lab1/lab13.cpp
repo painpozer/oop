80b2eb8d02586849260923fee68a9aab878bb2cc
Андрей Половинкин <polovinkin002@outlook.com>
2026-09-24 12:33:23 +0500

    обьявил структуру SafeArray

---
+++ lab1/lab13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @struct SafeArray
+ * @brief Хранит динамический массив и количество его элементов
+ */
+struct SafeArray {
+    int* data;
+    int size;
+};
+
+int main() {
+
+    return 0;
+}
---
+++ lab1/lab13.h
@@ -0,0 +1,16 @@
+//
+// Created by Андрей Половинкин on 24/9/26.
+//
+
+#ifndef LAB1_LAB13_H
+#define LAB1_LAB13_H
+
+
+
+class lab13 {
+
+};
+
+
+
+#endif //LAB1_LAB13_H
